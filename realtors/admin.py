b4dda8eacfdf6df5c95ce887f560ec34d89075c7
Daljeet Singh Chhabra <idaljeetsingh@outlook.com>
2019-12-21 12:36:13 +0530

    Completed search functionality with preserving form data

--- realtors/admin.py
+++ realtors/admin.py
@@ -5,7 +5,8 @@ from .models import Realtor
 class RealtorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'hire_date')
     list_display_links = ('id', 'name')
-    search_fields = ('name', )
+    search_fields = ('name',)
     list_per_page = 20
+
 
 admin.site.register(Realtor, RealtorAdmin)
